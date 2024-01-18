0f36dce45646f443a31f5c9b6e08894585ada2de
nasidfurqon <nasidfurqon@gmail.com>
2024-01-18 12:04:33 +0700

    change a content path on class and add a mtr and ass for spesific user

--- page/ass-user.php
+++ page/ass-user.php
@@ -1,11 +1,19 @@
 <!-- Tugas spesifik sesuai dengan ClassId dan UserId -->
 
     <!-- Card -->
-    <div class="card text-light rounded-start rounded-end">
-    <div class="bg rounded-start rounded-end"></div>
-    <div class="card-img-overlay">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+<div class="card-wrap mt-4">
+
+    <div class="card rounded-start rounded-end">
+        <div class="bg-ass rounded-start rounded-end border-secondary-subtle border border-1 border-opacity-10"></div>
+        <div class="card-img-overlay d-flex">
+            <div class="icon-wrap text-light me-3 ms-2 bg-success rounded-circle">
+                <i class="fa-regular fa-file-lines icon-file "></i>
+            </div>
+            <div class="title">
+                <h5 class="card-title fs-6">Card title</h5>
+                <p class="updated card-text text-black-50">Last updated 3 mins ago</p>
+            </div>
+        </div>
     </div>
-    </div>
+    
+</div>
--- page/class.php
+++ page/class.php
@@ -2,20 +2,20 @@
     <p >Class</p>
 </div>
 
-<div class="greeting mt-4">
+<div class="greeting mt-3">
   <p>Here are some classes available for you.</p>
 </div>
 
 <!-- Card -->
-<div class="row row-cols-1 pt-5 row-cols-md-3 g-4">
+<div class="row row-cols-1 pt-4 row-cols-md-3 g-4">
   <div class="col">
     <div class="card">
       <div class="bg card-img-top text-light d-flex">
         <div class="name p-3">
-          <a href="" class="fs-3 text-light text-decoration-none">Lorem ipsum dolor sit amet.</a>
+          <a href="index.php?page=in-class" class="fs-3 text-light text-decoration-none">Lorem ipsum dolor sit amet.</a>
         </div>
         <div class="list m-3 me-4 mt-4">
-          <p onclick="colOnClickMenu()" class="colon fa-solid fa-ellipsis-vertical text-light fa-xl float-end text-decoration-none mt-3"></p>
+          <p onclick="colOnClickMenu()" onmouseenter="colOnMouse(this)" onmouseleave="colOffMouse(this)" class="colon fa-solid fa-ellipsis-vertical text-light fa-xl float-end text-decoration-none mt-3"></p>
           <ul id="list-colon" class="list-colon list-unstyled border border-2 rounded-start rounded-end bg-light text-dark me-2">
             <li class="ps-2 pt-2 pe-2 list-colon-menu">Edit</li>
             <li class="p-2 list-colon-menu">Delete</li>
@@ -24,9 +24,10 @@
       </div>
       <div class="card-body">
         <p >Tenggat</p>
-        <h5 class="card-title">Homework</h5>
+        <a href="/index.php?page=assignment" class="hw card-title fs-3 text-decoration-none">Homework</a>
         <p class="card-text"></p>
       </div>
     </div>
   </div>
 </div>
+</script>
