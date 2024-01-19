9976e21df3ec087979951999093ab013174f2dbe
nasidfurqon <nasidfurqon@gmail.com>
2024-01-19 10:13:44 +0700

    add material  page

---
+++ page/material.php
@@ -0,0 +1,25 @@
+<!-- Untuk Semua Material dari UserId -->
+<div class="head border-dark fs-4 border-bottom">
+    <p >Material</p>
+</div>
+
+<h3 class="text-center pt-4 pb-3 title">Material from all classes</h3>
+
+<!-- Card -->
+
+<div class="card-wrap mt-4">
+
+    <div class="card rounded-start rounded-end">
+        <div class="bg-ass rounded-start rounded-end border-secondary-subtle border border-1 border-opacity-10"></div>
+        <div class="card-img-overlay d-flex">
+            <div class="icon-wrap text-light me-3 ms-2 rounded-circle">
+                <i class="fa-regular fa-file-lines icon-file "></i>
+            </div>
+            <div class="title">
+                <h5 class="card-title fs-6"><a href="/index.php?page=detail-mtr" class="text-decoration-none text-black">User memposting materi baru: </a></h5>
+                <p class="updated card-text text-black-50">Last updated 3 mins ago</p>
+            </div>
+        </div>
+    </div>
+    
+</div>
