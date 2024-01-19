b6a404d136d782e6c71b118fdb09aa767eaeb813
nasidfurqon <nasidfurqon@gmail.com>
2024-01-19 09:41:35 +0700

    add a detailed page for material

---
+++ page/detail-ass.php
@@ -0,0 +1,25 @@
+<div class="detail-ass">
+    <div class="title-detail d-flex mb-3 ">
+        <div class="icon-wrap text-light rounded-circle mt-1">
+            <a class="text-decoration-none text-light" href="/index.php?page=in-class">
+                <i class="fa-solid fa-question icon-file"></i>
+            </a>
+        </div>
+        <div class="title-detail border-bottom col-11 ms-4" id="title-detail">
+            <h1>Title</h1>
+            <div class="ms-2 updated-detail d-flex text-black-50">
+                <small><p>User | Updated</p></small>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <div class="content-detail-ass mt-4 me-3">
+        <div class="information mb-4">
+            <p class="fs-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ratione excepturi dolore. Quaerat dolor et ducimus modi, eius magni saepe aperiam recusandae, iusto possimus eos, dignissimos eveniet provident cupiditate magnam corrupti officia id at vitae quam impedit sint nulla quasi? Id quae modi corporis, reprehenderit soluta quos iste reiciendis et obcaecati. Saepe, deleniti natus. Sequi ipsa, placeat totam in pariatur porro id itaque quod commodi amet eius? Sunt assumenda placeat laudantium minus dolore et libero dolorum tempore ut quae eveniet, cupiditate, dicta molestiae at blanditiis ab ducimus eum consequuntur itaque nam aliquam nostrum. Obcaecati eligendi quam rem tempore quae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, dolorum.</p>
+        </div>
+        
+        <!-- PDF AND IMG FILE ONLY -->
+        <embed class="file" src="/file/Pemanduan2_Khoirul Nasid Furqon.pdf">
+    </div>
+</div>
