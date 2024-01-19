31bab72455a27cbb0689abf9c11cea858046ea82
nasidfurqon <nasidfurqon@gmail.com>
2024-01-19 10:11:55 +0700

    add assignment page

---
+++ page/assignment.php
@@ -0,0 +1,24 @@
+<!-- untuk semua tugas dari userID -->
+
+<div class="head border-dark fs-4 border-bottom">
+    <p >Assignment</p>
+</div>
+<h3 class="text-center pt-4 pb-3 title">Assignments from all classes</h3>
+
+<!-- Card -->
+<div class="card-wrap mt-4">
+
+    <div class="card rounded-start rounded-end">
+        <div class="bg-ass rounded-start rounded-end border-secondary-subtle border border-1 border-opacity-10"></div>
+        <div class="card-img-overlay d-flex">
+            <div class="icon-wrap text-light me-3 ms-2 rounded-circle">
+                <i class="fa-solid fa-question icon-file "></i>
+            </div>
+            <div class="title">
+                <h5 class="card-title fs-6"><a href="/index.php?page=detail-ass" class="text-decoration-none text-black">User memposting tugas baru: </a> </h5>
+                <p class="updated card-text text-black-50">Last updated 3 mins ago</p>
+            </div>
+        </div>
+    </div>
+    
+</div>
