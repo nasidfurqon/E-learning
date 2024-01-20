9937f946154d9e795941a74e6b56983d02c5b959
nasidfurqon <nasidfurqon@gmail.com>
2024-01-20 11:36:02 +0700

    change name file for detail page

--- page/detail-mtr.php
+++ page/detail-mtr.php
@@ -1,50 +1,25 @@
-<div class="row wrapper-detail">
-    <!-- left Side -->
-    <div class="col-sm-8 left-side">
-        <!-- Title -->
-        
-        <div class="title-detail d-flex mb-3 ">
-            <div class="icon-wrap text-light rounded-circle mt-1">
-                <a class="text-decoration-none text-light" href="/index.php?page=in-class&page2=mtr-user">
-                    <i class="fa-regular fa-file-lines icon-file"></i>
-                </a>
-            </div>
-            <div class="title-detail border-bottom col-11 ms-4" id="title-detail">
-                <h1>Title</h1>
-                <div class="ms-2 updated-detail d-flex text-black-50">
-                    <small><p>User | Updated</p></small>
-                </div>
-            </div>
+<div class="detail-ass">
+    <div class="title-detail d-flex mb-3 ">
+        <div class="icon-wrap text-light rounded-circle mt-1">
+            <a class="text-decoration-none text-light" href="/index.php?page=in-class&page2=mtr-user">
+                <i class="fa-regular fa-file-lines icon-file"></i>
+            </a>
         </div>
-        
-        <!-- Content -->
-        <div class="content-detail mt-4">
-            <div class="information mb-4">
-                <p class="fs-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ratione excepturi dolore. Quaerat dolor et ducimus modi, eius magni saepe aperiam recusandae, iusto possimus eos, dignissimos eveniet provident cupiditate magnam corrupti officia id at vitae quam impedit sint nulla quasi? Id quae modi corporis, reprehenderit soluta quos iste reiciendis et obcaecati. Saepe, deleniti natus. Sequi ipsa, placeat totam in pariatur porro id itaque quod commodi amet eius? Sunt assumenda placeat laudantium minus dolore et libero dolorum tempore ut quae eveniet, cupiditate, dicta molestiae at blanditiis ab ducimus eum consequuntur itaque nam aliquam nostrum. Obcaecati eligendi quam rem tempore quae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, dolorum.</p>
-            </div>
-
-            <!-- PDF AND IMG FILE ONLY -->
-            <embed class="file" src="/file/Pemanduan2_Khoirul Nasid Furqon.pdf">
-        </div>
-    </div>
-
-    <!-- right Side -->
-    <div class="right-side col-4">
-        <!-- Assignment -->
-        <div class="card ">
-            <div class="card-body">
-                <div class="info-card position-relative">
-                    <h5 class="card-title position-absolute start-0">Assignment</h5>
-                    <p class="text-black-50 position-absolute end-0">assigned</p>
-                </div>
-
-                <!-- Button -->
-                <div class="btn-wrapper">
-                    <a href="#" class="btn btn-transparent bg-transparent btn-primary position-relative mt-5" id="btn">+ Add Answer</a>
-                    <a href="#" class="btn text-light position-relative mt-3" id="btn">Marks as Done</a>
-                </div>
+        <div class="title-detail border-bottom col-11 ms-4" id="title-detail">
+            <h1>Title</h1>
+            <div class="ms-2 updated-detail d-flex text-black-50">
+                <small><p>User | Updated</p></small>
             </div>
         </div>
     </div>
-
+    
+    <!-- Content -->
+    <div class="content-detail-ass mt-4 me-3">
+        <div class="information mb-4">
+            <p class="fs-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus ratione excepturi dolore. Quaerat dolor et ducimus modi, eius magni saepe aperiam recusandae, iusto possimus eos, dignissimos eveniet provident cupiditate magnam corrupti officia id at vitae quam impedit sint nulla quasi? Id quae modi corporis, reprehenderit soluta quos iste reiciendis et obcaecati. Saepe, deleniti natus. Sequi ipsa, placeat totam in pariatur porro id itaque quod commodi amet eius? Sunt assumenda placeat laudantium minus dolore et libero dolorum tempore ut quae eveniet, cupiditate, dicta molestiae at blanditiis ab ducimus eum consequuntur itaque nam aliquam nostrum. Obcaecati eligendi quam rem tempore quae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, dolorum.</p>
+        </div>
+        
+        <!-- PDF AND IMG FILE ONLY -->
+        <embed class="file" src="/file/Pemanduan2_Khoirul Nasid Furqon.pdf">
+    </div>
 </div>
