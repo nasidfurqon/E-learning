128641319e5f7f6a780b50e7e18d24e159ef9b6b
nasidfurqon <nasidfurqon@gmail.com>
2024-01-20 11:46:08 +0700

    style for border

--- page/dashboard.php
+++ page/dashboard.php
@@ -15,7 +15,7 @@
             <p class="card-text icon">
                 <i class="fa-solid fa-school pe-1 fa-lg"></i>
             </p>
-            <a href="index.php?page=class" style="background-color: #00A9FF;" class="btn text-light mt-3 mb-2">7 Class</a>
+            <a href="index.php?page=class" id="btn-dashboard" class="btn border-primary-subtle bg-transparent mt-3 mb-2">7 Class</a>
         </div>
     </div>
     
@@ -25,7 +25,7 @@
             <p class="card-text icon ">
                 <i class="fa-solid fa-bookmark pe-2 fa-lg"></i>
             </p>
-            <a href="/index.php?page=assignment" style="background-color: #00A9FF;" class="btn text-light mt-3 mb-2">3 Assignment</a>
+            <a href="/index.php?page=assignment" id="btn-dashboard" class="btn border-primary-subtle mt-3 mb-2">3 Assignment</a>
         </div>
     </div>
     
@@ -35,7 +35,7 @@
             <p class="card-text icon">
                 <i class="fa-solid fa-inbox pe-1"></i>
             </p>
-            <a href="/index.php?page=announcement" style="background-color: #00A9FF;" class="btn text-light mt-3 mb-2">1 Announcement</a>
+            <a href="/index.php?page=announcement" id="btn-dashboard" class="btn border-primary-subtle mt-3 mb-2">1 Announcement</a>
         </div>
     </div>
 
