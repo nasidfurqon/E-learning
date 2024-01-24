a1ee2fb9320d4cf57359841b0f6e9085eb709b66
nasidfurqon <nasidfurqon@gmail.com>
2024-01-24 12:19:30 +0700

    add register page

--- page/register.php
+++ page/register.php
@@ -1,30 +1,34 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
+    <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/style/login.css">
-    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Caveat&family=Courgette&family=Dancing+Script&family=Exo+2:ital,wght@1,300&family=Handlee&family=Pangolin&family=Quicksand:wght@500&family=Shadows+Into+Light&family=Sono:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Caveat&family=Courgette&family=Dancing+Script&family=Exo+2:ital,wght@1,300&family=Handlee&family=Pangolin&family=Quicksand:wght@500&family=Shadows+Into+Light&family=Sono:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet"> 
 </head>
   <body>
     <div class="container-fluid">
-        <div class="card bg-transparent border-white position-absolute top-50 start-50 translate-middle" style="width: 18rem; height: 20.3rem;">
+        <div class="card bg-transparent border-white position-absolute top-50 start-50 translate-middle" style="width: 18rem; height: 23.9rem;">
             <div class="card-body">
-                <h5 class="card-title text-center pb-3 pt-3 text-white fw-bold fs-2">Login</h5>
+                <h5 class="card-title text-center pb-3 pt-3 text-white fw-bold fs-2">Register</h5>
                 <form action="page/login.php" method="post" enctype="multipart/form-data" class="p-2">
                     <div class="form-group mb-4 ">    
                         <span class="fa-solid fa-envelope position-absolute icon"></span>
                         <input type="text" name="email" id="email" class="form-control" placeholder="Email">
                     </div>
+                    <div class="form-group mb-4 ">    
+                        <span class="fa-solid fa-user    position-absolute icon"></span>
+                        <input type="text" name="username" id="username" class="form-control" placeholder="Username">
+                    </div>
                     <div class="form-group mb-4">
                         <span class="fa-solid fa-lock position-absolute icon"></span>
                         <input type="text" name="password" id="password" class="form-control" placeholder="Password">
                     </div> 
-                    <input type="submit" name="submit" value="Login" class="btn-login btn bg-white form-control fw-bold mb-1" style="width: 100%;">
+                    <input type="submit" name="submit" value="Register" class="btn-login btn bg-white form-control fw-bold mb-1" style="width: 100%;">
                 </form>
-                <small class="ps-4 text-white ">Don't have an account? <a href="register.php" class="text-white text-decoration-none">Register</a></small>
+                <small class="text-white back-login">Back to <a href="login.php" class="text-white text-decoration-none">Login</a></small>
             </div>
         </div>
     </div>
