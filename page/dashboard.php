216b643e55d6bcc562ef8da8541486c104ab3a29
nasidfurqon <nasidfurqon@gmail.com>
2024-01-29 11:24:54 +0700

    add a connection into database for dashboard page

--- page/dashboard.php
+++ page/dashboard.php
@@ -1,16 +1,34 @@
 <?php
-$id = isset($_GET['userId']) ? $_GET['userId'] : '';
 $conn = connect();
+// class
 $stmt = $conn->prepare("SELECT SUM(value) as sum FROM class WHERE userId = ?");
 $stmt->execute([$id]);
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $class = $stmt->fetch();
+
+// name
+$stmt2 = $conn->prepare("SELECT * FROM user WHERE id = ?");
+$stmt2->execute([$id]);
+$stmt2->setFetchMode(PDO::FETCH_ASSOC);
+$name = $stmt2->fetch();
+
+// assignment
+$stmt3 = $conn->prepare("SELECT SUM(value) as sum FROM assignment WHERE userid = ?");
+$stmt3->execute([$id]);
+$stmt3->setFetchMode(PDO::FETCH_ASSOC);
+$ass = $stmt3->fetch();
+
+// announce
+$stmt4 = $conn->prepare("SELECT SUM(value) as sum FROM announce WHERE userid = ?");
+$stmt4->execute([$id]);
+$stmt4->setFetchMode(PDO::FETCH_ASSOC);
+$announce = $stmt4->fetch();
 ?>
 <div class="head border-dark fs-4 border-bottom">
     <p >Dashboard</p>
 </div>
 <div class="greeting mt-4">
-    <p>Hello, Khoirul!</p>
+    <p>Hello, <?php echo $name['name'] ?></p>
     <p>Welcome to E-Learning Website</p>
 </div>
 
@@ -23,8 +41,14 @@ $class = $stmt->fetch();
             <p class="card-text icon">
                 <i class="fa-solid fa-school pe-1 fa-lg"></i>
             </p>
-            <a href="index.php?page=class" id="btn-dashboard" class="btn border-primary-subtle bg-transparent mt-3 mb-2">
-                <?php echo $class['sum'] ?> Class</a>
+            <a href="index.php?page=class&userId=<?php echo $id ?>" id="btn-dashboard" class="btn border-primary-subtle bg-transparent mt-3 mb-2">
+                <?php 
+            if ($class['sum'] == ''){
+                echo 0;
+            }
+            else{
+                echo $class['sum'];
+            } ?> Class</a>
         </div>
     </div>
     
@@ -34,7 +58,13 @@ $class = $stmt->fetch();
             <p class="card-text icon ">
                 <i class="fa-solid fa-bookmark pe-2 fa-lg"></i>
             </p>
-            <a href="/index.php?page=assignment" id="btn-dashboard" class="btn border-primary-subtle mt-3 mb-2">3 Assignment</a>
+            <a href="/index.php?page=assignment&userId=<?php echo $id ?>" id="btn-dashboard" class="btn border-primary-subtle mt-3 mb-2"><?php 
+            if ($ass['sum'] == ''){
+                echo 0;
+            }
+            else{
+                echo $ass['sum'];
+            }?> Assignment</a>
         </div>
     </div>
     
@@ -44,7 +74,13 @@ $class = $stmt->fetch();
             <p class="card-text icon">
                 <i class="fa-solid fa-inbox pe-1"></i>
             </p>
-            <a href="/index.php?page=announcement" id="btn-dashboard" class="btn border-primary-subtle mt-3 mb-2">1 Announcement</a>
+            <a href="/index.php?page=announcement&userId=<?php echo $id ?>" id="btn-dashboard" class="btn border-primary-subtle mt-3 mb-2"><?php
+            if ($announce['sum'] == ''){
+                echo 0;
+            }
+            else{
+                echo $announce['sum'];
+            } ?> Announcement</a>
         </div>
     </div>
 
