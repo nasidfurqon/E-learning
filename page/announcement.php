3dbfa79f99cdeac3194ea9b560f5f8fd7a9b4de0
nasidfurqon <nasidfurqon@gmail.com>
2024-02-02 09:22:51 +0700

    add a connection on announce page to database

--- page/announcement.php
+++ page/announcement.php
@@ -1,25 +1,49 @@
+<?php
+$conn = connect();
+$stmt = $conn->prepare("SELECT * FROM announce WHERE userid = ?");
+$stmt->execute([$id]);
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$announcements = $stmt->fetchAll();
+
+$stmt2 = $conn->prepare("SELECT * FROM user WHERE id = ?");
+$stmt2->execute([$id]);
+$stmt2->setFetchMode(PDO::FETCH_ASSOC);
+$user = $stmt2->fetch();
+?>
+
 <div class="head border-dark fs-4 border-bottom">
     <p >Announcement</p>
 </div>
-
 <!-- Card-Wrap -->
+<?php foreach($announcements as $announcement): ?>
+
 <div class="card-wrap mt-4">
 
     <!-- Card -->
     <div class="card rounded-start rounded-end p-3">
         <!-- Title -->
         <div class="Title-announce d-flex border-bottom m-3 pb-3" id="title-detail">
-            <img class="profile rounded-circle me-3" src="/img/Screenshot (360).png" alt="">
+            <img class="profile rounded-circle me-3" src="<?php echo $user['pp'] ?>" alt="">
             <div class="title">
-                <h5 class="card-title fs-6"><a href="/index.php?page=detail-mtr" class="text-decoration-none text-black">User memposting materi baru: </a></h5>
-                <p class="updated card-text text-black-50">Last updated 3 mins ago</p>
+                <h5 class="card-title fs-6"><a href="/index.php?page=detail-mtr" class="text-decoration-none text-black">User memposting pengumuman baru:<?php echo $announcement['title']?> </a></h5>
+                <p class="updated card-text text-black-50">Last updated 
+                    <?php 
+                    $date1 = $announcement['updated'];
+                    date_default_timezone_set("Asia/Jakarta");
+                    $date2 = date("h:i:sa");
+                    $diff = abs(strtotime($date2) - strtotime($date1));
+                    $minute = floor($diff/ 60);
+                    echo $minute;
+                    ?>
+                     mins ago</p>
             </div>
         </div>
 
         <!-- Content -->
         <div class="card-body">
-             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ipsa doloremque atque perferendis voluptates inventore, nobis veniam. Eligendi vel fugiat odit reiciendis facere rem, fugit nemo molestias incidunt voluptate recusandae velit corporis eaque consequatur perspiciatis officia pariatur iusto aut! Autem vel, sunt reprehenderit quae facere voluptate quis laborum et consectetur perferendis dolor ut omnis, doloribus nam, quam nulla fuga optio deleniti! Rerum recusandae quos autem neque nostrum nam cupiditate? Suscipit culpa ad aperiam adipisci officiis delectus fugit porro, neque, quasi quidem quisquam quibusdam obcaecati officia nobis asperiores ducimus nulla voluptatibus! Et eveniet veritatis corporis numquam, eos earum modi voluptatum quibusdam. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <p class="card-text"><?php echo $announcement['content'] ?></p>
         </div>
     </div>
     
 </div>
+<?php endforeach; ?>
