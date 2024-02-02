b6b4168b26652b797b5d2973e55a661ac757bfd6
nasidfurqon <nasidfurqon@gmail.com>
2024-02-02 07:46:42 +0700

    add a connection on assignment page to database

--- page/assignment.php
+++ page/assignment.php
@@ -1,3 +1,12 @@
+<?php
+$conn = connect();
+$stmt = $conn->prepare("SELECT * FROM assignment WHERE userid = ?");
+$stmt->execute([$id]);
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$assignments = $stmt->fetchAll();
+
+
+?>
 <!-- untuk semua tugas dari userID -->
 
 <div class="head border-dark fs-4 border-bottom">
@@ -6,6 +15,12 @@
 <h3 class="text-center pt-4 pb-2 title">Assignments from all classes</h3>
 
 <!-- Card -->
+<?php foreach($assignments as $assignment): 
+$stmt2 = $conn->prepare("SELECT * FROM class WHERE id = ?");
+$stmt2->execute([$assignment['classid']]);
+$stmt2->setFetchMode(PDO::FETCH_ASSOC);
+$class = $stmt2->fetch();
+?>
 <div class="card-wrap mt-4">
 
     <div class="card rounded-start rounded-end">
@@ -15,10 +30,20 @@
                 <i class="fa-solid fa-question icon-file "></i>
             </div>
             <div class="title">
-                <h5 class="card-title fs-6"><a href="/index.php?page=detail-ass" class="text-decoration-none text-black">User memposting tugas baru: </a> </h5>
-                <p class="updated card-text text-black-50">Last updated 3 mins ago</p>
+                <h5 class="card-title fs-6"><a href="/index.php?page=detail-ass&userId=<?php echo $id ?>&classId=<?php echo $class['id'] ?>&assId=<?php echo $assignment['id']?>" class="text-decoration-none text-black">user memposting tugas baru dari <?php echo $class['name'] ?>: <?php echo $assignment['name'] ?></a> </h5>
+                <p class="updated card-text text-black-50">Last updated
+                    <?php 
+                    $date1 = $assignment['updated'];
+                    date_default_timezone_set("Asia/Jakarta");
+                    $date2 = date("h:i:sa");
+                    $diff = abs(strtotime($date2) - strtotime($date1));
+                    $minute = floor($diff/ 60);
+                    echo $minute;
+                    ?>
+                     mins ago</p>
             </div>
         </div>
     </div>
     
 </div>
+<?php endforeach; ?>
