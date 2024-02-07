9412cbcee73211cb48835e549bfa61f2f4c5f8d1
nasidfurqon <nasidfurqon@gmail.com>
2024-02-07 08:33:20 +0700

    add profile page

---
+++ page/profil.php
@@ -0,0 +1,26 @@
+<div class="head border-dark fs-4 border-bottom">
+    <p >Profile</p>
+</div>
+
+
+<div class="container">
+    <div class="profile-content d-flex">
+        <div class="profile-picture">
+            <img src="/img/Screenshot (360).png" width="300px" height="300px" alt="">
+        </div>
+        <div class="biodata fs-5">
+            <div class="sub">
+                <p class="sub-bio">Name</p>
+                <p class="data"><b>:</b> khoirul</p>
+            </div>
+            <div class="sub">            
+                <p class="sub-bio">Email</p>
+                <p class="data"><b>:</b> Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="sub">
+                <p class="sub-bio">Address</p>
+                <p class="data"><b>:</b> tes</p>
+            </div>
+        </div>
+    </div>
+</div>
